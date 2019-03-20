4d88ea9910ff1efcce9d0fdc384775806e2572d7
Rynaard Burger <ryno.burger@gmail.com>
2019-03-20 22:40:40 +0100

    Add more documentation

---
+++ JazzyDocumentation/JazzyDocumentation/User.swift.swift
@@ -0,0 +1,15 @@
+/// Representation of a user
+public struct User: Codable {
+    let firstname: String
+    let lastname: String
+    let age: Int
+
+
+    /// User status
+    ///
+    /// - active: User is active
+    /// - blocked: User is blocked
+    enum Status {
+        case active, blocked
+    }
+}
--- JazzyDocumentation/JazzyDocumentation/UserApiClient.swift
+++ JazzyDocumentation/JazzyDocumentation/UserApiClient.swift
@@ -1,11 +1,3 @@
-//
-//  UserApiClient.swift
-//  JazzyDocumentation
-//
-//  Created by Rynaard Burger on 19.03.19.
-//  Copyright © 2019 Rynaard Burger. All rights reserved.
-//
-
 /**
  A simple HTTP user client
 
@@ -16,42 +8,32 @@
 
 import Foundation
 
-/// Representation of a user
-public struct User: Codable {
-    let firstname: String
-    let lastname: String
-    let age: Int
-
-
-    /// User status
-    ///
-    /// - active: User is active
-    /// - blocked: User is blocked
-    enum Status {
-        case active, blocked
-    }
-}
-
 /// User HTTP client
 public class UserApiClient {
+    /**
+     Creates a new user
+     - Parameter user: The new user
 
-    /// Creates a new user
-    ///
-    /// - Parameter user: The new user
-    public func newUser(_ user: User) {
+     ### Usage Example: ###
+     ````
+     UserApiClient.newUser(user1)
+     ````
 
+     */
+    public static func newUser(_ user: User) {
+        fatalError("not yet implemented")
     }
 
 
-    /// Creates a new user
+    /// Creates a new user (Deprecated)
     ///
     /// - Parameters:
     ///   - firstname: User firstname
     ///   - lastname: User lastname
     ///   - age: User age
-    /// - warning: Deprecated. Use: `UserApiClient.newUser(_ user: User)`
+    /// - warning: Deprecated. Use: `newUser(_ user: User)`
     @available(*, deprecated, message: "Use newUser(_ user: User)")
-    public func newUser(firstname: String, lastname: String, age: Int) {
-
+    public static func newUser(firstname: String, lastname: String, age: Int) {
+        fatalError("not yet implemented")
     }
 }
